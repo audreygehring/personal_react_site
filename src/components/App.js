79bbcfca33d3f93e69e6aae59cf6be132d9346d2
Audrey Gehring <audreygehring@gmail.com>
2017-01-21 14:26:28 -0500

    separated about and contact components from app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,54 @@
 import React from 'react';
+import About from './About';
+import Contact from './Contact';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      aboutClicked: false,
+      contactClicked: false
+    };
+    this.handleAboutClick = this.handleAboutClick.bind(this);
+    this.handleContactClick = this.handleContactClick.bind(this);
   }
+
+  handleAboutClick(event) {
+    if (this.state.aboutClicked === false) {
+      this.setState({ aboutClicked: true });
+    } else {
+      this.setState({ aboutClicked: false });
+    }
+  }
+
+  handleContactClick(event) {
+    if (this.state.contactClicked === false) {
+      this.setState({ contactClicked: true });
+    } else {
+      this.setState({ contactClicked: false });
+    }
+  }
+
   render () {
+    let aboutClick = () => {
+      this.handleAboutClick();
+    };
+
+    let contactClick = () => {
+      this.handleContactClick();
+    };
     return (
       <div>
+        <h1> Audrey Gehring </h1>
+        <About
+          clickedStatus={this.state.aboutClicked}
+          aboutClick={aboutClick}
+        />
+
+        <Contact
+          clickedStatus={this.state.contactClicked}
+          contactClick={contactClick}
+        />
       </div>
     );
   }
---
+++ src/components/Contact.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Contact = props => {
+  let contactAudrey;
+  if (props.clickedStatus == false ) {
+    contactAudrey = "";
+  } else {
+    contactAudrey = "Github Logo - LinkedIn Logo";
+  }
+
+  return (
+    <div>
+      <div>
+        <h2 onClick={props.contactClick}>Contact</h2>
+        <h3> {contactAudrey} </h3>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
